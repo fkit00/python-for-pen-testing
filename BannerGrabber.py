8a6182d1d250b56de8300ad2c0b2c14baef8ff2e
fiona <fiona.kitchen@yahoo.co.uk>
2023-01-24 10:56:18 +0000

    banner grabber complete

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -2,10 +2,27 @@
 
 import socket 
 ## we want user to enter ip adresss and port 
-s = socket.socket()
+#s = socket.socket()
+#ip = input ("Please enter the IP: ")
+#port = str(input("Please enter the port: "))
+#s.connect((ip, int(port)))
+#print(s.recv(1024))  ## calling the recive and we're going to limit it to a certain number of bytes
 
-ip = input ("Please enter the IP: ")
-port = str(input("Please enter the port: "))
+## re-write in a function 
 
-s.connect((ip, int(port)))
-print(s.recv(1024))  ## calling the recive and we're going to limit it to a certain number of bytes
+
+def banner(ip,port):
+    s= socket.socket()
+    s.connect((ip, int(port)))
+    s.settimeout(5)
+    print(str(s.recv(1024)).strip('b')) ## this strip uncessary output the b but this might cause some funkiness later on 
+
+
+def main():
+    ip = input ("Please enter the IP: ")
+    port = str(input("Please enter the port: "))
+    banner(ip,port)
+
+
+main()    
+  
