0fad8e1ccc07e86bc3ded6b54efe21557ab9cfdb
fiona <fiona.kitchen@yahoo.co.uk>
2023-01-24 11:30:31 +0000

    just added inputs to make it more flexible and a timeout!

--- portScanner.py
+++ portScanner.py
@@ -1,6 +1,10 @@
 import socket 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ## ipv4, sockstream spesifies the type of connection TCP not UDP
+s.settimeout(5)
+
+host = input ("Please enter the IP you want to scan: ")
+port = int(input("Please enter the port you would like to scan: "))
 
 
 
@@ -9,3 +13,5 @@ def portScanner(port):
            print("This port is closed")
     else:
         print("The port is open")   
+
+portScanner(port)
